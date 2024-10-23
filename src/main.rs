b7ce8653ea59363966d36806b5984da8cb50ec1e
mufeng05 <gjilkiop@qq.com>
2024-10-23 15:19:55 +0800

    Refactor system command and file writing in main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,33 @@ use std::fs::File;
 use std::io::Write;
 use std::process::Command;
 
+fn write_to_file(output: String) -> Result<(), std::io::Error> {
+    let mut file = File::create("output.txt")?;
+    file.write_all(output.as_bytes())?;
+    Ok(())
+}
+
+fn get_birdc_output() -> Result<String, std::io::Error> {
+    let output = Command::new("birdc").arg("s").arg("o").arg("s").output()?;
+    let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+    match write_to_file(stdout.clone()) {
+        Ok(_) => {
+            println!("Command output saved to output.txt");
+        }
+        Err(e) => {
+            println!("Failed to write to file: {}", e);
+        }
+    }
+    Ok(stdout)
+}
+
 fn main() {
-    // 执行系统命令
-    let output = Command::new("birdc")
-        .arg("s")
-        .arg("o")
-        .arg("s")
-        .output()
-        .expect("Failed to execute command");
-
-    // 将输出转换为字符串
-    let stdout = String::from_utf8_lossy(&output.stdout);
-
-    // 打开文件并写入输出
-    let mut file = File::create("output.txt").expect("Failed to create file");
-    file.write_all(stdout.as_bytes())
-        .expect("Failed to write to file");
-
-    println!("{}", stdout);
-    println!("Command output saved to output.txt");
+    match get_birdc_output() {
+        Ok(output) => {
+            println!("{}", output);
+        }
+        Err(e) => {
+            println!("Failed to get birdc output: {}", e);
+        }
+    }
 }
