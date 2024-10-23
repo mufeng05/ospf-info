49dd85770385d789b19d151a3c43823be7682479
mufeng05 <gjilkiop@qq.com>
2024-10-23 15:46:46 +0800

    Refactor system command and file writing in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::fs::File;
+use axum::{self, http::StatusCode, routing::get, Router};
 use std::io::Write;
 use std::process::Command;
+use std::{fs::File, io::Read};
 
 fn write_to_file(output: String) -> Result<(), std::io::Error> {
     let mut file = File::create("output.txt")?;
@@ -11,6 +12,7 @@ fn write_to_file(output: String) -> Result<(), std::io::Error> {
 fn get_birdc_output() -> Result<String, std::io::Error> {
     let output = Command::new("birdc").arg("s").arg("o").arg("s").output()?;
     let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+    println!("Get birdc output successfully");
     match write_to_file(stdout.clone()) {
         Ok(_) => {
             println!("Command output saved to output.txt");
@@ -22,13 +24,38 @@ fn get_birdc_output() -> Result<String, std::io::Error> {
     Ok(stdout)
 }
 
-fn main() {
+async fn ospf_status() -> (StatusCode, String) {
     match get_birdc_output() {
-        Ok(output) => {
-            println!("{}", output);
-        }
+        Ok(output) => (StatusCode::OK, output),
         Err(e) => {
             println!("Failed to get birdc output: {}", e);
+            let mut file_content = String::new();
+            match File::open("output.txt") {
+                Ok(mut file) => match file.read_to_string(&mut file_content) {
+                    Ok(_) => {
+                        return (StatusCode::OK, file_content);
+                    }
+                    Err(read_err) => {
+                        println!("Failed to read from file: {}", read_err);
+                    }
+                },
+                Err(open_err) => {
+                    println!("Failed to open file: {}", open_err);
+                }
+            }
+            (StatusCode::INTERNAL_SERVER_ERROR, String::new())
         }
     }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Starting server on");
+
+    let app = Router::new().route("/", get(ospf_status));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:55300").await?;
+    axum::serve(listener, app.into_make_service()).await?;
+
+    Ok(())
 }
